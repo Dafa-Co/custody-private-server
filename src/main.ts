61b30bd56658fb641df962cb6f0448a85a3f2b93
A7medAref <ahmedmostafa992001@gmail.com>
2025-02-23 12:11:40 +0200

    setup the project needed constants

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import configs from './utils/configs/configs';
 import { AppModule } from './app.module';
 import { getConsumerConfig } from 'rox-custody_common-modules/libs/config/rmq.config';
 import { CustodyLogger } from 'rox-custody_common-modules/libs/services/logger/custody-logger.service';
+import { setupNeededConstants } from 'rox-custody_common-modules/libs/utils/setup-needed-constants';
 
 
 async function bootstrap() {
+  setupNeededConstants({ projectName: 'PS' });
+
   const app = await NestFactory.create(AppModule);
 
   const logger = app.get(CustodyLogger);
-  
+
   app.useLogger(logger)
 
   // Create RabbitMQ microservice
