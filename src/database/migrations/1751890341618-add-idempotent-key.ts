c2781edf774a325310810e27f890462071bde906
MoazHassan2022 <moaaz.mohamed@dafagate.com>
2025-07-20 11:19:53 +0300

    move eoa address migration

--- src/database/migrations/1751890341618-add-idempotent-key.ts
+++ src/database/migrations/1751890341618-add-idempotent-key.ts
@@ -6,11 +6,9 @@ export class AddIdempotentKey1751890341618 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`idempotent_key_entity\` (\`idempotentKey\` varchar(255) NOT NULL, \`keyId\` int NULL, \`address\` varchar(255) NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), UNIQUE INDEX \`REL_f3f38f77919f13b90c982fbfa0\` (\`keyId\`), PRIMARY KEY (\`idempotentKey\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`idempotent_key_entity\` ADD CONSTRAINT \`FK_f3f38f77919f13b90c982fbfa02\` FOREIGN KEY (\`keyId\`) REFERENCES \`private_keys\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`idempotent_key_entity\` ADD \`eoaAddress\` varchar(255) NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`idempotent_key_entity\` DROP COLUMN \`eoaAddress\``);
         await queryRunner.query(`ALTER TABLE \`idempotent_key_entity\` DROP FOREIGN KEY \`FK_f3f38f77919f13b90c982fbfa02\``);
         await queryRunner.query(`DROP INDEX \`REL_f3f38f77919f13b90c982fbfa0\` ON \`idempotent_key_entity\``);
         await queryRunner.query(`DROP TABLE \`idempotent_key_entity\``);
