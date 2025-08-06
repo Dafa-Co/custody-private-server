adf41ad827456b17e2a7c3624976ac9abea9a419
hossamghoneim <hossamghoneim11@gmail.com>
2025-08-06 12:41:09 +0300

    Polkadot

--- src/blockchain/different-networks/polkadot-strategy.service.ts
+++ src/blockchain/different-networks/polkadot-strategy.service.ts
@@ -1,7 +1,7 @@
-import { CustodySignedTransaction } from "rox-custody_common-modules/libs/interfaces/custom-signed-transaction.type";
+import { CustodySignedTransaction, SignedPolkadotTransaction } from "rox-custody_common-modules/libs/interfaces/custom-signed-transaction.type";
 import { PrivateServerSignTransactionDto, SignTransactionDto } from "rox-custody_common-modules/libs/interfaces/sign-transaction.interface";
 import { IBlockChainPrivateServer, InitBlockChainPrivateServerStrategies, IWalletKeys } from "../interfaces/blockchain.interface";
-import { CommonAsset } from "rox-custody_common-modules/libs/entities/asset.entity";
+import { AssetType, CommonAsset } from "rox-custody_common-modules/libs/entities/asset.entity";
 import { Chain } from "viem";
 import { getChainFromNetwork } from "rox-custody_common-modules/blockchain/global-commons/get-network-chain";
 import {
@@ -15,6 +15,9 @@ import { hexToU8a, u8aToHex } from '@polkadot/util';
 import Decimal from 'decimal.js';
 import BigNumber from 'bignumber.js'
 import type { AccountInfo } from '@polkadot/types/interfaces';
+import { InternalServerErrorException } from "@nestjs/common";
+import { softJsonStringify } from "rox-custody_common-modules/libs/utils/soft-json-stringify.utils";
+import { CustodyLogger } from "rox-custody_common-modules/libs/services/logger/custody-logger.service";
 
 export class PolkadotStrategyService implements IBlockChainPrivateServer {
     private asset: CommonAsset;
@@ -35,7 +38,18 @@ export class PolkadotStrategyService implements IBlockChainPrivateServer {
 
         const provider = new WsProvider(this.host);
 
-        this.api = await ApiPromise.create({ provider });
+        //this.api = await ApiPromise.create({ provider });
+        this.api = await ApiPromise.create({
+            provider,
+            types: {
+              // Optional: Add any custom runtime types here
+            },
+            signedExtensions: {
+                customExtension1: { extrinsic: {}, payload: {} },
+                customExtension2: { extrinsic: {}, payload: {} }
+            },
+            // Removed userExtensions as it is not a valid property of ApiOptions
+        });
         this.keyring = new Keyring({ type: 'sr25519' });
     }
 
@@ -58,31 +72,75 @@ export class PolkadotStrategyService implements IBlockChainPrivateServer {
         const { amount, to, transactionId } = dto;
 
         try {
-            const keyring = new Keyring({ type: 'sr25519' });
-            // Convert hex to Uint8Array
-            const privateKeySeed = hexToU8a(privateKey);
-            // Add from seed (assumes it's a valid 32-byte secret seed)
-            const sender = keyring.addFromSeed(privateKeySeed);
-            const decimalAmount = new Decimal(amount);
-            const transfer = this.api.tx.balances.transferKeepAlive(to, (new BigNumber(decimalAmount.toJSON())).toString());
-            const accountInfo = await this.api.query.system.account(sender.address);
-            const { nonce } = accountInfo as AccountInfo;
-            const signedTx = await transfer.signAsync(sender, { nonce });
+            let signedTransaction: SignedPolkadotTransaction;
+
+            switch (this.asset.type) {
+                case AssetType.COIN:
+                    signedTransaction = await this.getSignedTransactionCoin(
+                        privateKey,
+                        to,
+                        amount,
+                    );
+                    break;
+
+                default:
+                    throw new InternalServerErrorException('Polkadot only supports coins');
+            }
+
 
             return {
                 bundlerUrl: this.host,
-                signedTransaction: signedTx.toHex(),
+                signedTransaction: signedTransaction,
                 error: null,
-                transactionId,
+                transactionId: transactionId,
             };
         } catch (error) {
             return {
                 bundlerUrl: this.host,
                 signedTransaction: null,
                 error: error.message,
-                transactionId,
+                transactionId: transactionId,
             };
         }
+    }
+
+    private async getSignedTransactionCoin(
+        privateKey: string,
+        to: string,
+        amount: Decimal,
+    ): Promise<SignedPolkadotTransaction> {
+        const keyring = new Keyring({ type: 'sr25519' });
+        // Convert hex to Uint8Array
+        const privateKeySeed = hexToU8a(privateKey);
+        // Add from seed (assumes it's a valid 32-byte secret seed)
+        const sender = keyring.addFromSeed(privateKeySeed);
+        const decimalAmount = new Decimal(amount);
+        const transfer = this.api.tx.balances.transferKeepAlive(to, (new BigNumber(decimalAmount.toJSON())).toString());
+        const accountInfo = await this.api.query.system.account(sender.address);
+        const { nonce } = accountInfo as AccountInfo;
+        const signedTx = await transfer.signAsync(sender, { nonce });
+        const currentBlock = (await this.api.rpc.chain.getHeader()).number.toNumber();
+        const eraPeriod = 64;
+
+        const validityStart = currentBlock - (currentBlock % eraPeriod);
+        const validityEnd = validityStart + eraPeriod;
+
+        if (!signedTx) {
+            const logger = new CustodyLogger();
+
+            logger.notification(`Transaction signature not found for ${softJsonStringify(transfer)}`);
+
+            throw new InternalServerErrorException('Error while getting signature');
+        }
+
+        console.log(`Current Block: ${currentBlock}, Validity End Block: ${validityEnd}`);
+
+
+        return {
+            hash: signedTx.toHex(),
+            currentBlock,
+            endBlock: validityEnd
+        };
     }
 
     getSignedSwapTransaction(dto: SignTransactionDto, privateKey: string): Promise<CustodySignedTransaction> {
