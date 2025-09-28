1ca8d9680112bc9fdf87535796768044a348ddfc
MoazHassan2022 <moaaz.mohamed@dafagate.com>
2025-09-28 14:41:43 +0300

    merge with stage

--- src/blockchain/blockchain.module.ts
+++ src/blockchain/blockchain.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BlockchainFactoriesService } from './blockchain-strategies.service';
-import { BitcoinStrategyService } from './different-networks/bitcoin-strategy.service';
-import { TronStrategyService } from './different-networks/tron-strategy.service';
 import { NonceManagerModule } from 'src/nonce-manager/nonce-manager.module';
-import { AccountAbstractionStrategyService } from './different-networks/account-abstraction/account-abstraction-strategy.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrivateKeyVersion } from 'src/keys-manager/entities/private-key-version.entity';
 
--- src/utils/interfaces/convert-private-key-to-smart-account-result.interface.ts
+++ src/utils/interfaces/convert-private-key-to-smart-account-result.interface.ts
@@ -1,5 +1,5 @@
+import { ISmartAccount } from "src/blockchain/different-networks/evm/account-abstraction/interfaces/smart-account.interface";
 import { BiconomyAccountTypeEnum } from "../enums/biconomy-account-type.enum";
-import { ISmartAccount } from "src/blockchain/different-networks/account-abstraction/interfaces/smart-account.interface";
 
 export interface IConvertPrivateKeyToSmartAccountResult {
     account: ISmartAccount;
